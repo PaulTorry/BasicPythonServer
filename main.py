af31b50c6e6bcd61f42ca1811a8cdd0e5786323a
Paul T <PaulTorry@users.noreply.github.com>
2025-04-25 16:47:59 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import socket
+
+print('hi')
+
+addr = socket.getaddrinfo('0.0.0.0', 8080)[0][-1]
+s = socket.socket()
+s.bind(('0.0.0.0', 8080))
+s.listen(3)
+print('Listening: ',addr )
+
+
+try:
+    while True:
+        con, add = s.accept()
+        print(con, add)
+        data = con.recv(1024)
+        print(data)
+        con.send(b'HTTP/1.0 200 OK\r\nContrnt-type: text/html\r\n\r\n')
+        con.sendall(b'hi')
+        con.sendall(data)
+        #print(data.find.__str__('GET')) 
+finally:
+    s.close()
